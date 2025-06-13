8883548d595cefb430b8f7416f11fa935a9981fc
Almanassik-Alarabi <almanassik.alarabi@gmail.com>
2025-06-13 20:23:52 +0100

    Update config.js

--- public/admin/includes/config.js
+++ public/admin/includes/config.js
@@ -1,5 +1,5 @@
     // إعداد Supabase
-      const supabaseUrl = "https://zrwtxvybdxphylsvjopi.supabase.co";
+      const supabaseUrl = "https://xkfucayxbqpszezppbbc.supabase.co";
       const supabaseKey =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpyd3R4dnliZHhwaHlsc3Zqb3BpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyMzM1NzIsImV4cCI6MjA2NDgwOTU3Mn0.QjdaZ5AjDJEgu7rNIY4gnSqzEww0VXJ4DeM3RrykI2s";
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrZnVjYXl4YnFwc3plenBwYmJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0Njc0MDUsImV4cCI6MjA2NTA0MzQwNX0.HPz1Vh0aRlIk5tOUoaDbcYaKt1DydKn7L9K2YaORDqA";
       const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
